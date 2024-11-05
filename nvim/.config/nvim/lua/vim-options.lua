933e99e5591e2a8ecbcddef9f556856ec08af786
arnemar <arne.m@telenet.be>
2024-11-05 20:46:39 +0100

    added mouse support for nvim cursor placement and copy paste to and from clipboard

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set number")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set mouse=a")
 vim.g.mapleader = " "
-
-
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
